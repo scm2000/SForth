cbe38ee8470685285048fe35709b0dca3bd346da
Stephen Morgana <stephen@morgana.cc>
2021-04-04 20:18:39 -0400

    add basic support for compilation of user defined functions to machine code

--- SForth.cpp
+++ SForth.cpp
@@ -1,27 +1,24 @@
 #include <sys/types.h>
+#include <stdio.h>
 #include <setjmp.h>
 #include <Arduino.h>
 
 const char *eol = "\r\n";
 
 #include "SForth.h"
-static void outputString(char *str, ...)
+static void outputFormat(char *fmt, ...)
 {
-  Serial.print(str);
+  char tmpStr[1000];
   va_list valist;
-  va_start(valist, str);
-  char *nextStr = va_arg(valist, char *);
-  while (nextStr)
-  {
-    Serial.print(nextStr);
-    nextStr = va_arg(valist, char *);
-  }
+  va_start(valist, fmt);
+  vsnprintf(tmpStr, sizeof(tmpStr), fmt, valist);
   va_end(valist);
+  Serial.print(tmpStr);
 }
 
 //#define DEBUG
 #ifdef DEBUG
-#define DEBUG_PRINT(string) outputString("SForth DEBUG: ", string,eol, 0);
+#define DEBUG_PRINT(string) outputFormat("SForth DEBUG: %s%s", string,eol);
 #else
 #define DEBUG_PRINT(string)
 #endif
@@ -63,7 +60,7 @@ static void printError(errorCode code)
       codeWord = "unexpected error code";
       break;
   }
-  outputString("Error: ", codeWord, ", ", errorMessage, eol, 0);
+  outputFormat("Error: %s, %s%s", codeWord, errorMessage, eol);
 }
 
 /* malloc stuff */
@@ -88,16 +85,12 @@ typedef struct dictElt {
   uint8_t type;
   char name[MAX_TOKEN_LEN + 1];
   dictElt *prev;
-  union {
-    uint32_t numVal;
-    void (*preDefFunc)(void);
-    uint32_t funcStart;
-  } val;
+  uint32_t val;
 };
 
 static dictElt *dictHead = 0;
 
-static void dictDefine(char *name, dictEltType type, void (*funcPtr)(void))
+static void dictDefine(char *name, dictEltType type, void (*funcPtr)(void) = 0, uint32_t *code = 0, int len = 0)
 {
   DEBUG_PRINT("dictDefine called");
 
@@ -108,7 +101,7 @@ static void dictDefine(char *name, dictEltType type, void (*funcPtr)(void))
       {
         DEBUG_PRINT("defining a variable");
         newElt = (dictElt*)mallocMem(sizeof(dictElt));
-        newElt->val.numVal = 0;
+        newElt->val = 0;
       }
       break;
 
@@ -116,7 +109,22 @@ static void dictDefine(char *name, dictEltType type, void (*funcPtr)(void))
       {
         DEBUG_PRINT("defining a predefined function");
         newElt = (dictElt*)mallocMem(sizeof(dictElt));
-        newElt->val.preDefFunc = funcPtr;
+        newElt->val = (uint32_t)funcPtr;
+      }
+      break;
+    case function:
+      {
+        DEBUG_PRINT("defining user defined function");
+        newElt = (dictElt*)mallocMem(sizeof(dictElt) + len - sizeof(uint32_t));
+#ifdef DEBUG
+        outputFormat("funcStart: %08x%s", &newElt->val, eol);
+#endif
+        //copy code over
+        memcpy(&newElt->val, code, len);
+
+#ifdef DEBUG
+        outputFormat("stored code: %08x%s",  newElt->val, eol);
+#endif
       }
       break;
     default:
@@ -165,6 +173,11 @@ static uint32_t *dStackPtr = firstDStackBlock.stack + STACK_BLOCK_SIZE;
 
 void dStackPush(uint32_t val)
 {
+  DEBUG_PRINT("dStackPushed was in fact called");
+#ifdef DEBUG
+  outputFormat("val was: %d%s", val, eol);
+#endif
+
   if (dStackPtr == dStackHead->stack)
   {
     // overflowing this block, alloc a new one
@@ -216,7 +229,7 @@ static void nextToken()
   while (*cptr && !isspace(*cptr))
   {
 #ifdef DEBUG
-    outputString("Scan state: ", cptr, eol, 0);
+    outputFormat("Scan state: %s%s", cptr, eol);
 #endif
 
     if (tptr - curToken < MAX_TOKEN_LEN)
@@ -228,7 +241,7 @@ static void nextToken()
 
   *tptr = '\0';
 #ifdef DEBUG
-  outputString("FinalToken: ", curToken, eol, 0);
+  outputFormat("FinalToken: %s%s", curToken, eol);
 #endif
 }
 
@@ -236,23 +249,17 @@ static void nextToken()
 static void printUnsignedDecimalValue()
 {
   uint32_t val = dStackPop();
-  char valStr[12];
-  sprintf(valStr, "%u%s", val, eol);
-  outputString(valStr, 0);
+  outputFormat("%u%s", val, eol);
 }
 static void printSignedDecimalValue()
 {
   uint32_t val = dStackPop();
-  char valStr[12];
-  sprintf(valStr, "%d%s", val, eol);
-  outputString(valStr, 0);
+  outputFormat("%d%s", val, eol);
 }
 static void printHexValue()
 {
   uint32_t val = dStackPop();
-  char valStr[12];
-  sprintf(valStr, "0x%08x%s", val, eol);
-  outputString(valStr, 0);
+  outputFormat("0x%08x%s", val, eol);
 }
 
 static void sfAdd()
@@ -324,9 +331,171 @@ static void sfVariable()
   }
 }
 
+// this code is just to get things going.. needs future optimizations
+// A function to pass a 32 bit number to dStackPush
+// the code snip will look like:
+// 4802     load r0 with number 8 bytes away
+// 4b02     load r3 with address 8 bytes away
+// 4798     branch link through r3
+// bf00     nop (because data has to be a multiple of 4 away)
+// e002     branch over next 8 bytes
+// xxxx low 16 bits of value
+// yyyy hight 16 bits of value
+// wwww low 16 bits of dStackPush address
+// zzzz high 16 bits of dStackPush address
+// nop for allignment of next instruction.
+const size_t insertDStackPushHWCount = 10; // be good about keeping this correct
+static void insertDStackPush(uint16_t *&codePtr, uint32_t val)
+{
+  uint32_t callLoc = (uint32_t)&dStackPush;
+  *codePtr++ = 0x4802;
+  *codePtr++ = 0x4b02;
+  *codePtr++ = 0x4798;
+  *codePtr++ = 0xbf00;
+  *codePtr++ = 0xe002;
+  *codePtr++ = (val) & 0xffff;
+  *codePtr++ = (val) >> 16;
+  *codePtr++ = callLoc & 0xffff;
+  *codePtr++ = callLoc >> 16;
+  *codePtr++ = 0xbf00;
+}
+
+// A function to call a function or predefined
+// the code snip will look like:
+// 4b01     load r3 with address 4 bytes away
+// 4798     branch link through r3
+// e001     branch over next 4 bytes
+// wwww low 16 bits of dStackPush address
+// zzzz high 16 bits of dStackPush address
+// nop for allignment of next instruction.
+const size_t insertCallToAddrHWCount = 6;
+static void insertCallToAddr(uint16_t *&codePtr, uint32_t addr)
+{
+  uint32_t callLoc = (uint32_t)&dStackPush;
+  *codePtr++ = 0x4b01;
+  *codePtr++ = 0x4798;
+  *codePtr++ = 0xe001;
+  *codePtr++ = (addr) & 0xffff;
+  *codePtr++ = (addr) >> 16;
+  *codePtr++ = 0xbf00;
+}
+
+static uint32_t tokenToNumber();
+static void reserveHalfWords(uint16_t *&codeStart, uint16_t *&codePtr, size_t &totalAvailHW, size_t HWPerChunk, size_t halfWordsNeeded)
+{
+  if ((codePtr + halfWordsNeeded) - codeStart > totalAvailHW)
+  {
+    totalAvailHW += HWPerChunk;
+    size_t offsetSave = codePtr - codeStart;
+    codeStart = (uint16_t*)reallocf(codeStart, totalAvailHW * sizeof(uint16_t));
+    if (!codeStart)
+    {
+      myThrow(cantMalloc, "out of memory allocating temp code space");
+    }
+    codePtr = codeStart + offsetSave;
+  }
+}
 static void sfDefineFunction()
 {
   // grab tokens up to a ; and compile to machine code, woo hoo!
+  // uses a temp buffer to build the function, then copies into
+  // the dictionary.  Allocate chunks of bytes at a time, may realloc
+  size_t HWPerChunk = 512;
+  size_t totalAvailHW = HWPerChunk;
+  uint16_t *codeStart = (uint16_t*)realloc(0, totalAvailHW * sizeof(uint16_t));
+  if (!codeStart)
+    myThrow(cantMalloc, "out of memory allocating temp code space");
+
+  uint16_t *codePtr = codeStart;
+
+  // function prolog is to push {r3, lr}
+  *codePtr++ = 0xb508;
+
+  nextToken(); // better be the word we are defining
+  if (!isalpha(curToken[0]))
+    myThrow(notAWord, ": defined word must start with a letter");
+
+  char name[33];
+  strcpy(name, curToken);
+
+  nextToken();
+
+  while (curToken[0] && curToken[0] != ';')
+  {
+    if (isdigit(curToken[0]))
+    {
+      reserveHalfWords(codeStart, codePtr, totalAvailHW, HWPerChunk, insertDStackPushHWCount);
+
+      uint32_t num = tokenToNumber();
+      insertDStackPush(codePtr, num);
+    }
+    else // assume a word
+    {
+      // find it in the dictionary
+      dictElt *entry = dictLookup(curToken);
+      if (entry)
+      {
+        // found in dict
+        switch ((dictEltType)entry->type)
+        {
+          case variable:
+            {
+              DEBUG_PRINT("token is a variable reference");
+
+              reserveHalfWords(codeStart, codePtr, totalAvailHW, HWPerChunk, insertDStackPushHWCount);
+
+              uint32_t valAddr = (uint32_t)&entry->val;
+              insertDStackPush(codePtr, valAddr);
+            }
+            break;
+
+          case function:
+            {
+              DEBUG_PRINT("token is a function reference");
+
+              reserveHalfWords(codeStart, codePtr, totalAvailHW, HWPerChunk, insertCallToAddrHWCount);
+
+              uint32_t offsetStart = ((uint32_t)&entry->val) | 0x1;
+              insertCallToAddr(codePtr, offsetStart);
+            }
+            break;
+
+          case predefinedFunction:
+            {
+              DEBUG_PRINT("token is a predefined function reference");
+
+              reserveHalfWords(codeStart, codePtr, totalAvailHW, HWPerChunk, insertCallToAddrHWCount);
+
+              insertCallToAddr(codePtr, entry->val);
+            }
+            break;
+
+          default:
+            DEBUG_PRINT("dict is trashed?");
+            myThrow (internalError, "apparently the dictionary is trashed");
+            break;
+        }
+
+      }
+      else // undefined word
+      {
+        myThrow (undefinedWord, curToken);
+      }
+    }
+
+    nextToken();
+  }
+
+  // now the prolog for the function.
+  // for now we pop return.. (We could have just jumped to last function call.. todo)
+  *codePtr++ = 0xbd08;  // this is pop {r3, pc}
+  *codePtr++ = 0xbf00;  // this is nop
+#ifdef DEBUG
+  outputFormat("codeStartStuff: %08x", *((uint32_t*)codeStart));
+#endif
+  dictDefine(name, function, 0, (uint32_t*)codeStart, (codePtr - codeStart) * 2);
+
+  free(codeStart);
 
 }
 
@@ -345,6 +514,15 @@ static void sfDigitalWrite()
   uint32_t b = dStackPop();
   digitalWrite(b, a);
 
+}
+// 01 e0                          b #2 <$t+0x4>  branches over 4 bytes
+//4f f0 11 30                    mov.w r0, #286331153    loads a constant into register 0
+volatile int a;
+static void sampleOp()
+{
+  ((void (*)(uint32_t))0xdeadbeef)(0x12345678);
+  dStackPop();
+  dStackPop();
 }
 
 ///////////////////SForth entry points
@@ -375,20 +553,38 @@ void SForthBegin()
   dictDefine("dup", predefinedFunction, sfDup);
   dictDefine("swap", predefinedFunction, sfSwap);
   dictDefine("variable", predefinedFunction, sfVariable);
+  dictDefine(":", predefinedFunction, sfDefineFunction);
+  dictDefine("so", predefinedFunction, sampleOp);
 
-  Serial.println("SForth is up and running!");
+  outputFormat("SForth is up and running!%s", eol);
 #ifdef DEBUG
   // test the stack
   dStackPush(123);
   uint32_t a = dStackPop();
   if (a == 123)
-    outputString("d stack seems ok", 0);
+    outputFormat("d stack seems ok");
   else
-    outputString("d stack did not work", 0);
+    outputFormat("d stack did not work");
 #endif
 
 }
 
+static uint32_t tokenToNumber()
+{
+  // some kind of numeric string
+  char *numFormat;
+
+  if (curToken[1] == 'x')
+    numFormat = "0x%x";
+  else
+    numFormat = "%d";
+
+  DEBUG_PRINT("token is:");
+  DEBUG_PRINT(curToken);
+  uint32_t val;
+  sscanf(curToken, numFormat, &val);
+  return val;
+}
 void SForthEvaluate(char *str)
 {
   DEBUG_PRINT("Evaluate called");
@@ -410,18 +606,7 @@ void SForthEvaluate(char *str)
     {
       DEBUG_PRINT("token is a number");
 
-      // some kind of numeric string
-      char *numFormat;
-      if (curToken[1] == 'x')
-        numFormat = "0x%x";
-      else
-        numFormat = "%d";
-
-      DEBUG_PRINT("token is:");
-      DEBUG_PRINT(curToken);
-      uint32_t val;
-      sscanf(curToken, numFormat, &val);
-      dStackPush(val);
+      dStackPush(tokenToNumber());
     }
     else // must be a word
     {
@@ -438,19 +623,33 @@ void SForthEvaluate(char *str)
           case variable:
             DEBUG_PRINT("token is a variable reference");
             // push the address of the variable's numVal;
-            dStackPush((uint32_t)&entry->val.numVal);
+            dStackPush((uint32_t)&entry->val);
             break;
 
           case function:
-            DEBUG_PRINT("token is a function reference");
-            // wow.. call the function code packed in the dictElt
-            ((void (*)(void))((&entry->val.funcStart) + 1))();
+            {
+              DEBUG_PRINT("token is a function reference");
+              // wow.. call the function code packed in the dictElt
+              volatile void (*fptr)(void);
+
+              uint32_t offsetStart = ((uint32_t)&entry->val) | 0x1;
+#ifdef DEBUG
+              outputFormat("fooCode: %08x%s", entry->val, eol);
+              uint16_t *dumpLoc = (uint16_t*)&entry->val;
+              for (int i = 0; i < 12; ++i)
+              {
+                outputFormat("dump %d: %04x%s", i, *dumpLoc++, eol);
+              }
+#endif
+              fptr = (volatile void(*)(void))(offsetStart);
+              fptr();
+            }
             break;
 
           case predefinedFunction:
             DEBUG_PRINT("token is a predefined function reference");
             // call through the function pointer
-            entry->val.preDefFunc();
+            ((void (*)(void))entry->val)();
             break;
 
           default:
@@ -478,7 +677,7 @@ bool prompted = false;
 static void shellHook() {
   if (!prompted)
   {
-    outputString("SForth> ", 0);
+    outputFormat("SForth> ");
     prompted = true;
   }
 
@@ -496,13 +695,13 @@ static void shellHook() {
         break;
 
       case 0xd:
-        Serial.print("\r\n");
+        Serial.print(eol);
         *lptr = '\0';
         SForth.evaluate(lineBuf);
         lptr = lineBuf;
         prompted = false;
         break;
-        
+
       default:
         if (c > '\x1f' && c < '\x7f')
         {
